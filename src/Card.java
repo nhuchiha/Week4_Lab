1a604e214766e50b7137cc7a092e4799b37b2825
nhuchiha <nhuchiha2019@gmail.com>
2020-08-01 12:20:08 -0700

    Update Card

--- src/Card.java
+++ src/Card.java
@@ -1,97 +1,94 @@
 //Exercise #3
-
 import java.util.Random;
-
 public class Card 
 {
 	//Declare variables for face and suit values
 	static int faceValue;
 	static int suitValue;
-
+			
 	public static void main(String[] args) 
 	{
-		// create card object with face and suit value
-		 Card card = new Card(faceValue, suitValue);
-		 Random generator = new Random();
-		 
-		 //Generate 5 cards
-		 for (int i = 0; i < 5; i++)
-		 {
-			 faceValue = generator.nextInt(13) + 1;
-			 suitValue = generator.nextInt(4) + 1;
-			 System.out.print(card);
-		 }
+		//Create card object with face and suit Value
+		Card card = new Card(faceValue, suitValue);
+		Random generator = new Random();
+		
+		//Generate 5 cards
+		for(int i = 0; i < 5; i++)
+		{
+			faceValue = generator.nextInt(13) + 1;
+			suitValue = generator.nextInt(4) + 1;
+			System.out.print(card);
+		}
+
 	}
 	
 	//Convert those suit and face values to string
 	public String toString()
 	{
 		String card = "";
-	 	switch (faceValue)
-	 	{
-		   case 1: 
-			   card += " A";
-		       break;
-		   case 2: 
-			   card += " 2";
-		       break;
-		   case 3: 
-			   card += " 3";
-		       break;
-		   case 4: 
-			   card += " 4";
-		       break;
-		   case 5: 
-			   card += " 5";
-		       break;    
-		   case 6: 
-			   card += " 6";
-		       break;     
-		   case 7: 
-			   card += " 7";
-		       break;    
-		   case 8: 
-			   card += " 8";
-		       break;    
-		   case 9: 
-			   card += " 9";
-		       break;    
-		   case 10: 
-			   card += " 10";
-		       break;    
-		   case 11:
-			   card += " J";
-		       break;
-		   case 12:
-			   card += " Q";
-		       break;
-		   case 13:
-			   card += " K";
-	       		break;
-	 	}
-
-	 	switch (suitValue)
-	 	{
-	 		case 1:
-	   			card +='S';
-	   			break;
-	   		case 2:	
-	   			card +='C';
-	   			break;
-	   		case 3: 
-	   			card +='D';
-	   			break;
-	   		case 4: 
-	   			card +='H';
-	   			break;
-	   	}
-	 	return card;
+		switch (faceValue)
+		{
+			case 1: 
+				card = card + " A";
+				break;
+			case 2: 
+				card = card + " B"; 
+				break;
+			case 3: 
+				card = card + " 3";
+				break;
+			case 4: 
+				card = card + " 4";
+				break;
+			case 5: 
+				card = card + " 5";
+				break;
+			case 6: 
+				card = card + " 6";
+				break;
+			case 7:
+				card = card + " 7";
+			case 8: 
+				card = card + " 8";
+			case 9:
+				card = card + " 9";
+				break;
+			case 10:
+				card = card + " 10";
+				break;
+			case 11:
+				card = card + " J";
+				break;
+			case 12:
+				card = card + " Q";
+				break;
+			case 13:
+				card = card + " K";
+				break;		
+		}
+		
+		switch (suitValue)
+		{
+			case 1: 
+				card = card + "S";
+				break;
+			case 2:
+				card = card + "C";
+				break;
+			case 3:
+				card = card + "D";
+				break;
+			case 4:
+				card = card + "H";
+		}
+		return card;
 	}
 	
-	// Create Class Card with 2 parameters for faceValue and suitValue
+	//Create class Card with 2 parameters for faceValue and suitValue
 	public Card(int suit, int face)
 	{
 		faceValue = suit;
 		suitValue = face;
 	}
+
 }
